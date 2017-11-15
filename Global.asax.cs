13e24a4b24955781a3ec1882628ec63210971c2e
Venkatesh Chunchu <Venkateshchunchu@gmail.com>
2017-11-15 16:22:57 +0530

    Logging Exceptions
    Logger file helps us to log the exception details into database, text file , Event Viewer and we can also send the logs to emails.
    
    Exceptions can be caught at the 
    1) Snippet level(using try catch)
    2) Page level(Page_Error() event)
    3) Application Level(Application_ Error() event)
    
    Every Exception should be cleared after we catch them otherwise it will show us yellow death screen.
    
    We can define custom errors in the web.config file based on their status codes
    We can define our credentials in web.config file rather than passing throw code behind file.

---
+++ Global.asax.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using System.Web.SessionState;
+
+namespace ASPNetDemoWebApplication
+{
+    public class Global : System.Web.HttpApplication
+    {
+        protected void Application_Start(object sender, EventArgs e)
+        {
+             
+            Application["TotalApplications"] = 0;
+            Application["TotalUserSessions"] = 0;
+
+            Application["TotalApplications"] = (int)Application["TotalApplications"] + 1;
+        }
+        protected void Application_End(object sender, EventArgs e)
+        {
+
+        }
+        protected void Application_Error(object sender, EventArgs e)
+        {
+            Exception ex = Server.GetLastError();
+            if (ex != null)
+            {
+                Logger.Log(ex);
+                Server.ClearError();
+                Response.Redirect("~/Error.aspx");
+            }
+        }
+        
+        protected void Session_Start(object sender, EventArgs e)
+        {
+            Application.Lock();
+            Application["TotalUserSessions"] = (int)Application["TotalUserSessions"] + 1;
+            Application.UnLock();
+        }
+        protected void Session_End(object sender, EventArgs e)
+        {
+            Application.Lock();
+            Application["TotalUserSessions"] = (int)Application["TotalUserSessions"] - 1;
+            Application.UnLock();
+        }
+    }
+}
---
+++ Logger.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Net.Mail;
+using System.IO;
+
+namespace ASPNetDemoWebApplication
+{
+    public class Logger
+    {
+
+        public static void Log(Exception exception)
+        {
+            Log(exception, EventLogEntryType.Error);
+        }
+        public static void Log(Exception exception, EventLogEntryType eventLogEntryType)
+        {
+            var sbExceptionMessage = new StringBuilder();
+            do
+            {
+                sbExceptionMessage.Append("Exception Type: " + Environment.NewLine);
+                sbExceptionMessage.Append(exception.GetType().Name + Environment.NewLine);
+                sbExceptionMessage.Append("/n" + Environment.NewLine);
+                sbExceptionMessage.Append("Message: " + Environment.NewLine);
+                sbExceptionMessage.Append(exception.Message + Environment.NewLine);
+                sbExceptionMessage.Append(Environment.NewLine + Environment.NewLine);
+                sbExceptionMessage.Append("Stack Trace: " + Environment.NewLine);
+                sbExceptionMessage.Append(exception.StackTrace + Environment.NewLine);
+
+                exception = exception.InnerException;
+            } while (exception != null);
+
+            string logProvider = ConfigurationManager.AppSettings["LogProvider"];
+            if (logProvider.ToLower() == "database")
+            {
+                LogToDB(sbExceptionMessage.ToString());
+            }
+            else if (logProvider.ToLower() == "eventviewer")
+            {
+                LogToEventViewer(sbExceptionMessage.ToString(), eventLogEntryType);
+            }
+            else if (logProvider.ToLower() == "SendEmail")
+            {
+                SendLogToEmail(sbExceptionMessage.ToString();
+            }
+            else
+            {
+                LogToDB(sbExceptionMessage.ToString());
+                LogToEventViewer(sbExceptionMessage.ToString(), eventLogEntryType);
+                SendLogToEmail(sbExceptionMessage.ToString());
+            }
+        }
+
+        private static void LogToEventViewer(string sbExceptionMessage, EventLogEntryType eventLogEntryType)
+        {
+            if (!EventLog.SourceExists("ApplicationLogs.com"))
+            {
+                // if the source is not there in the event viewer then create one
+                EventLog.CreateEventSource("ApplicationLogs.com", "ApplicationLogs");
+            }
+            else
+            {
+                EventLog log = new EventLog("ApplicationLogs");
+                log.Source = "ApplicationLogs.com";
+
+                log.WriteEntry(sbExceptionMessage.ToString(), eventLogEntryType);
+            }
+        }
+        private static void LogToDB(String sbExceptionMessage)
+        {
+            string conString = ConfigurationManager.ConnectionStrings["DBConnectionString"].ConnectionString;
+            SqlConnection con = new SqlConnection(conString);
+            SqlCommand cmd = new SqlCommand("spInsertLog", con);
+            cmd.CommandType = System.Data.CommandType.StoredProcedure;
+
+            SqlParameter param = new SqlParameter("@ExceptionMessage", sbExceptionMessage.ToString());
+            cmd.Parameters.Add(param);
+
+            con.Open();
+            cmd.ExecuteNonQuery();
+            con.Close();
+        }
+        private static void SendLogToEmail(string emailBody)
+        {
+            System.Net.Mail.MailMessage mailMessage = new MailMessage("venk120soft@gmail.com", "venk120soft@gmail.com");
+            mailMessage.Subject = "Exception";
+            mailMessage.Body = emailBody;
+
+            SmtpClient smtpClient = new SmtpClient();
+            smtpClient.Send(mailMessage);
+        }
+        private static void LogToTextFile(string sbExceptionMessage)
+        {
+            //Using System.IO
+            string filepath = "c:/somefile.txt";//context.current.server
+            if (!Directory.Exists(filepath))
+            {
+                Directory.CreateDirectory(filepath);
+            }
+            filepath = filepath + DateTime.Today.ToString() + ".txt";
+            if (File.Exists(filepath))
+            {
+                File.Create(filepath).Dispose();
+            }
+            using (StreamWriter sw = File.AppendText(filepath))
+            {
+                sw.WriteLine("Log Wriiten date --Details On-- " + DateTime.Now.ToString());
+                sw.WriteLine(sbExceptionMessage);
+                sw.Flush();
+                sw.Close();
+            }
+        }
+    }
+}
